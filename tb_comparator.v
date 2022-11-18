0cced44ad94d810cac77b0966ec90195dd9df930
4sun1 <87072962+Birdlovesbaby@users.noreply.github.com>
2022-11-18 16:05:52 +0900

    Update tb_comparator.v

--- tb_comparator.v
+++ tb_comparator.v
@@ -8,6 +8,7 @@ wire signed [1:0] out;
 initial begin
     #1 in1 = 16'habcd; in2 = 16'hcabd;
     #1 in1 = 16'hcaca; in2 = 16'h9999;
+    #1 in =  16'heeee; in2 = 16'heeee;
     #1
     $finish;
 end
